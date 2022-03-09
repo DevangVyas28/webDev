feb970bef6d70e308b48c80aa56af4f4d3c283d5
DevangVyas28 <devangnvyas@gmail.com>
2022-03-09 11:49:41 +0530

    Calculator with a backend!

--- introToNode/index.js
+++
@@ -1,8 +0,0 @@
-var superheroes = require("superheroes");
-var supervillains = require("supervillains");
-
-var mySuperheroName = superheroes.random();
-var myVillainName = supervillains.random();
-
-console.log(mySuperheroName);
-console.log(myVillainName);
