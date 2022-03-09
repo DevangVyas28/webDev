8e5e2c17d48a21706abb6954b23b67ddb0108855
DevangVyas28 <devangnvyas@gmail.com>
2022-03-09 09:20:03 +0530

    started with node

---
+++ introToNode/index.js
@@ -0,0 +1,8 @@
+var superheroes = require("superheroes");
+var supervillains = require("supervillains");
+
+var mySuperheroName = superheroes.random();
+var myVillainName = supervillains.random();
+
+console.log(mySuperheroName);
+console.log(myVillainName);
