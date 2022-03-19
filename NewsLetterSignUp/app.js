f30b8a499b641d73e1cbd0d3103299f6fbe2d910
DevangVyas28 <devangnvyas@gmail.com>
2022-03-19 16:58:37 +0530

    NewsLetter SignUp

---
+++ NewsLetterSignUp/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname+'/signup.html')
+});
+
+app.post("/", function(req,res){
+    var name = req.body.name;
+    var email = req.body.email;
+    console.log(name);
+    console.log(email);
+})
+
+
+
+
+
+
+
+app.listen(3000)
