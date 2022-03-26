1788fb8c0a3d3160fe4cf4f37d43bee8d6feed75
DevangVyas28 <devangnvyas@gmail.com>
2022-03-26 12:09:09 +0530

    ToDoList done

--- ToDoList/app.js
+++ ToDoList/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser")
 let tasks = [];
+let workTask = []
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static("public"))
 app.set("view engine", "ejs");
 
 
@@ -16,14 +18,27 @@ app.get('/', function(req,res){
     };
     let day = today.toLocaleDateString("en-UD", options);
     
-    res.render("list", {dayOfWeek: day, addTask: tasks})
+    res.render("list", {listTitle: day, addTask: tasks})
 });
 
 app.post("/", function(req, res){
     task = req.body.newTask;
-    tasks.push(task);
-    res.redirect("/")
+    if (req.body.list === "Work Task") {
+        workTask.push(task);
+        res.redirect("/work")
+    }
+    else {
+        tasks.push(task);
+        res.redirect("/")
+    };
+
+    
 });
+
+app.get("/work", function(req,res){
+    res.render("list", {listTitle: "Work Task", addTask: workTask})
+
+})
 
 
 app.listen(3000);
